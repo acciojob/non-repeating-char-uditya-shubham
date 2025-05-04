90dc2485bd60962205fe8a1c5045ad339f27557d
Uditya Kumar <shubhamkumaar2001@gmail.com>
2025-05-04 12:50:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const charCount = {};
+
+ 
+  for (let char of str) {
+    
+    if (charCount[char] === undefined) {
+      charCount[char] = 0;
+    }
+    charCount[char] += 1;
+  }
+
+  // Find the first character with count === 1
+  for (let char of str) {
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+
+  return null; 
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
